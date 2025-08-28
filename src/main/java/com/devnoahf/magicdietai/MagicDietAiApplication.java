9831bb9f315eca68cc96236d541a033e6d65a011
Noah Vêri Franco <noahvf16@outlook.com>
2025-08-28 14:10:11 -0300

    initial commit

---
+++ src/main/java/com/devnoahf/magicdietai/MagicDietAiApplication.java
@@ -0,0 +1,13 @@
+package com.devnoahf.magicdietai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicDietAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicDietAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devnoahf/magicdietai/MagicDietAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devnoahf.magicdietai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicDietAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
